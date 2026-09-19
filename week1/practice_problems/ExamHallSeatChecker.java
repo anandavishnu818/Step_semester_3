7b346a660537b11f8d91f42dacd5850b2f8cb1e7
anandavishnu818 <anandvishnu.j@gmail.com>
2026-09-19 11:06:41 +0530

    Create ExamHallSeatChecker.java

---
+++ week1/practice_problems/ExamHallSeatChecker.java
@@ -0,0 +1,28 @@
+public class ExamHallSeatChecker {
+
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean foundDuplicate = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    foundDuplicate = true;
+                    break; // avoid printing the same duplicate multiple times
+                }
+            }
+        }
+
+        if (!foundDuplicate) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] hall1 = {101, 102, 103, 102, 105};
+        int[] hall2 = {101, 102, 103, 104, 105};
+
+        checkDuplicateSeats(hall1);
+        checkDuplicateSeats(hall2);
+    }
+}
